3e07ce2d3f37c71fe1c4905fb47fb2a67e698bb0
Brodie Gilmour <brodiegilmour@hotmail.co.uk>
2018-08-25 16:11:44 +0100

    Adding vallidation to the angle

--- CannonAttack.Tests/Tests.cs
+++ CannonAttack.Tests/Tests.cs
@@ -1,6 +1,7 @@
 ﻿using NUnit.Framework;
 using CannonAttack;
 using System;
+using NUnit.Framework.Constraints;
 
 namespace CannonAttack.Tests
 {
@@ -17,12 +18,36 @@ namespace CannonAttack.Tests
         public void Given_AnAngle_When_SettingTheAngle_Then_AngleShouldBeSet()
         {
             var cannon = new Cannon();
-            var angle = 0;
+            var angle = 45;
 
             cannon.Angle = angle;
 
             Assert.AreEqual(angle, cannon.Angle);
         }
+
+
+        [Test]
+        public void Given_AnAngleLessThan1Degree_When_SettingTheAngle_Then_ArgumentOutOfRangeExceptionShouldBeThrown()
+        {
+            var cannon = new Cannon();
+            var angle = 0;
+
+            void testDelegate() => cannon.Angle = angle;
+
+            Assert.Throws<ArgumentOutOfRangeException>(testDelegate);
+        }
+
+        [Test]
+        public void Given_AnAngleGreaterThan89Degrees_When_SettingTheAngle_Then_ArgumentOutOfRangeExceptionShouldBeThrown()
+        {
+            var cannon = new Cannon();
+            var angle = 90;
+
+            void testDelegate() => cannon.Angle = angle;
+
+            Assert.Throws<ArgumentOutOfRangeException>(testDelegate);
+        }
+
 
         [Test]
         public void Given_ASpeed_When_SettingTheSpeed_Then_SpeedShouldBeSet()
--- CannonAttack/Cannon.cs
+++ CannonAttack/Cannon.cs
@@ -8,7 +8,17 @@ namespace CannonAttack
     {
         public const string ID = "HUMAN";
 
-        public double Angle;
+        private double _angle;
+
+        public double Angle {
+            get { return _angle; }
+            set {
+                if (value < 1) throw new ArgumentOutOfRangeException("Angle cannot be less than 1 degree.");
+                if (value > 89) throw new ArgumentOutOfRangeException("Angle cannot be 90 degrees or larger.");
+                _angle = value;
+            }
+        }
+
         public double Speed;
 
         public Cannon() { }
